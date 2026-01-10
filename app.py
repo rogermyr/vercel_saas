8fb6b6d2b9d45ab99eeaa08c3ed34f6ee0dd9831
Rogério Henriques Silva <rogerio.silva@gmail.com>
2026-01-09 23:40:05 -0300

    Adicionando actions

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 # Importa as funções (certifique-se de ter o __init__.py na pasta api)
 from api.crawler import run_process as run_crawler
 from api.item_collector import handle_item_collector as run_items
@@ -18,6 +18,10 @@ def health_check():
 
 @app.route('/api/cron/sync-tudo')
 def sync_tudo():
+    # Verifica se a senha enviada pelo GitHub é igual à que está na Vercel
+    auth_header = request.headers.get('Authorization')
+    if auth_header != f"Bearer {os.getenv('CRON_SECRET')}":
+        abort(401) # Erro de não autorizado
     logger.info("🔄 Iniciando Sincronização Geral")
     try:
         db_url = os.getenv("DATABASE_URL")
